1d30c3e15ba5a87ae95bd4bebfda475f2cb5e1db
DullCat <cat_cxy@qq.com>
2023-09-07 10:32:01 +0800

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: DullCat
+ * @Date: 2023-09-07 10:31:25
+ * @Description: Not Final Decision
+ */
+/*
+ * @Author: DullCat
+ * @Date: 2023-09-07 10:22:08
+ * @Description: Not Final Decision
+ */
+
+function log(msg) {
+	console.log('log: ', msg);
+}
+export default log;
